415fb6c062df79c259d12e04ed09ab88d656e6f0
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2025-01-03 13:51:19 +0100

    feat: rework audio play function to pass all fcc tests

--- App_.jsx
+++ App_.jsx
@@ -8,42 +8,95 @@ export default function App() {
 	const [sessionLength, setSessionLength] = useState(25);
 	const [breakLength, setBreakLength] = useState(5);
 	const [isBreakInProgress, setBreakInProgress] = useState(false);
+	const [paused, setPaused] = useState(true);
+	const [over, setOver] = useState(false);
+	const [[h, m, s], setTime] = useState([0, sessionLength, 0]);
+	const [[hb, mb, sb], setBreakTime] = useState([0, breakLength, 0]);
+
+	const tick = () => {
+		if (paused) return;
+		if (h === 0 && m === 0 && s === 0) {
+			let over = true;
+			setOver(over);
+			const beep = document.getElementById("beep");
+			beep.play();
+		} else if (m === 0 && s === 0) {
+			setTime([h - 1, 59, 59]);
+		} else if (s === 0) {
+			setTime([h, m - 1, 59]);
+		} else {
+			setTime([h, m, s - 1]);
+		}
+	};
+
+	function reset() {
+		setTime([0, sessionLength, 0]);
+		setBreakTime([0, breakLength, 0]);
+		setPaused(true);
+		setOver(false);
+	}
+
+	useEffect(() => {
+		const timerID = setInterval(() => tick(), 1000);
+		return () => clearInterval(timerID);
+	});
 
 	function increaseSession() {
 		if (sessionLength < 60) {
-			setSessionLength(sessionLength + 1);
+			const newSessionLength = sessionLength + 1;
+			setSessionLength(newSessionLength);
+			const newTime = [h, newSessionLength, s];
+			setTime(newTime);
 		}
 	}
 
 	function decreaseSession() {
 		if (sessionLength > 1) {
-			setSessionLength(sessionLength - 1);
+			const newSessionLength = sessionLength - 1;
+			setSessionLength(newSessionLength);
+			const newTime = [h, newSessionLength, s];
+			setTime(newTime);
 		}
 	}
 
 	function increaseBreak() {
 		if (breakLength < 60) {
-			setBreakLength(breakLength + 1);
+			const newBreakLength = breakLength + 1;
+			setBreakLength(newBreakLength);
+			const newBreakTime = [h, newBreakLength, s];
+			setBreakTime(newBreakTime);
 		}
 	}
 
 	function decreaseBreak() {
 		if (breakLength > 1) {
-			setBreakLength(breakLength - 1);
+			const newBreakLength = breakLength - 1;
+			setBreakLength(newBreakLength);
+			const newBreakTime = [h, newBreakLength, s];
+			setBreakTime(newBreakTime);
 		}
 	}
 
 	return (
 		<>
+			{/* biome-ignore lint/a11y/useMediaCaption: <explanation> */}
+			<audio
+				src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+				id="beep"
+			/>
 			<h1>25 + 5 Clock</h1>
 			<div className="timers">
 				<div className="timeControls">
 					<h3 className="sectionHeading">Session Length</h3>
-					<Arrows
-						functionUp={increaseSession}
-						functionDown={decreaseSession}
-						time={sessionLength}
-					/>
+					<div className="arrows">
+						<button type="button" onClick={increaseSession}>
+							<i className="fa-solid fa-arrow-up" />
+						</button>
+						<h3>{sessionLength}</h3>
+						<button type="button" onClick={decreaseSession}>
+							<i className="fa-solid fa-arrow-down" />
+						</button>
+					</div>
 				</div>
 				<div className="timeControls">
 					<h3 className="sectionHeading">Break Length</h3>
@@ -58,15 +111,26 @@ export default function App() {
 					</div>
 				</div>
 			</div>
-
-			<CountDown minutes={sessionLength} />
-			{
-				//{!isBreakInProgress ? (
-				//			<CountDown minutes={sessionLength} />
-				//		) : (
-				//			<CountDownBreak minutes={breakLength} />
-				//)}
-			}
+			<div id="countdown-timer">
+				<p>{`${m
+					.toString()
+					.padStart(2, "0")}:${s.toString().padStart(2, "0")}`}</p>
+				<div>{over ? "Break time!" : ""}</div>
+				<div>
+					<button
+						type="button"
+						onClick={() => {
+							const newPaused = !paused;
+							setPaused(newPaused);
+						}}
+					>
+						{paused ? "Start|Resume" : "Pause"}
+					</button>
+					<button type="button" onClick={() => reset()}>
+						Restart
+					</button>
+				</div>
+			</div>
 		</>
 	);
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,193 @@
 import React, { useState, useEffect } from "react";
-import CountDown from "./CountDown";
-import CountDownBreak from "./CountDownBreak";
-import Arrows from "./Arrows";
+
 import "./App.css";
 
 export default function App() {
 	const [sessionLength, setSessionLength] = useState(25);
 	const [breakLength, setBreakLength] = useState(5);
-	const [isBreakInProgress, setBreakInProgress] = useState(false);
+	const [paused, setPaused] = useState(true);
+	const [[m, s], setTime] = useState([sessionLength, 0]);
+	const [[mb, sb], setBreakTime] = useState([breakLength, 0]);
+	const [isBreakInProgress, setIsBreakInProgress] = useState(false);
 
 	function increaseSession() {
 		if (sessionLength < 60) {
-			setSessionLength(sessionLength + 1);
+			const newSessionLength = sessionLength + 1;
+			setSessionLength(newSessionLength);
+			const newTime = [newSessionLength, s];
+			setTime(newTime);
 		}
 	}
 
 	function decreaseSession() {
 		if (sessionLength > 1) {
-			setSessionLength(sessionLength - 1);
+			const newSessionLength = sessionLength - 1;
+			setSessionLength(newSessionLength);
+			const newTime = [newSessionLength, s];
+			setTime(newTime);
 		}
 	}
 
 	function increaseBreak() {
 		if (breakLength < 60) {
-			setBreakLength(breakLength + 1);
+			const newBreakLength = breakLength + 1;
+			setBreakLength(newBreakLength);
+			const newBreakTime = [newBreakLength, s];
+			setBreakTime(newBreakTime);
 		}
 	}
 
 	function decreaseBreak() {
 		if (breakLength > 1) {
-			setBreakLength(breakLength - 1);
+			const newBreakLength = breakLength - 1;
+			setBreakLength(newBreakLength);
+			const newBreakTime = [newBreakLength, s];
+			setBreakTime(newBreakTime);
 		}
 	}
 
+	function tick() {
+		if (paused) return;
+		if (m === 0 && s === 0) {
+			setBreakTime([breakLength, 0]);
+			const breakInProgress = true;
+			setIsBreakInProgress(breakInProgress);
+
+			return;
+		}
+		if (s === 0) {
+			let mins = m - 1;
+			let secs = 59;
+			setTime([mins, secs]);
+		} else {
+			let mins = m;
+			let secs = s - 1;
+			setTime([mins, secs]);
+		}
+	}
+
+	function tickBreak() {
+		if (paused) return;
+		if (mb === 0 && sb === 0) {
+			setTime([sessionLength, 0]);
+			const breakInProgress = false;
+			setIsBreakInProgress(breakInProgress);
+
+			return;
+		}
+		if (sb === 0) {
+			let mins = mb - 1;
+			let secs = 59;
+			setBreakTime([mins, secs]);
+		} else {
+			let mins = mb;
+			let secs = sb - 1;
+			setBreakTime([mins, secs]);
+		}
+	}
+
+	function reset() {
+		const audio = document.getElementById("beep");
+		audio.pause();
+		audio.load();
+		setSessionLength(25);
+		setBreakLength(5);
+		setTime([25, 0]);
+		setBreakTime([5, 0]);
+		setPaused(true);
+		setIsBreakInProgress(false);
+	}
+
+	useEffect(() => {
+		if (isBreakInProgress) {
+			const timerID = setInterval(() => tickBreak(), 1000);
+			return () => clearInterval(timerID);
+		}
+		const timerID = setInterval(() => tick(), 1000);
+		return () => clearInterval(timerID);
+	});
+
+	useEffect(() => {
+		if ((mb === 0 && sb === 0) || (s === 0 && m === 0)) {
+			document.getElementById("beep").play();
+		}
+	});
+
 	return (
 		<>
+			{/* biome-ignore lint/a11y/useMediaCaption: <explanation> */}
+			<audio
+				src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+				id="beep"
+			/>
 			<h1>25 + 5 Clock</h1>
 			<div className="timers">
 				<div className="timeControls">
-					<h3 className="sectionHeading">Session Length</h3>
+					<h3 className="sectionHeading" id="session-label">
+						Session Length
+					</h3>
 					<div className="arrows">
 						<button
 							type="button"
-							onClick={() => setSessionLength(sessionLength + 1)}
+							onClick={increaseSession}
+							id="session-increment"
 						>
 							<i className="fa-solid fa-arrow-up" />
 						</button>
-						<h3>{sessionLength}</h3>
-						<button type="button" onClick={decreaseSession}>
+						<h3 id="session-length">{sessionLength}</h3>
+						<button
+							type="button"
+							onClick={decreaseSession}
+							id="session-decrement"
+						>
 							<i className="fa-solid fa-arrow-down" />
 						</button>
 					</div>
 				</div>
 				<div className="timeControls">
-					<h3 className="sectionHeading">Break Length</h3>
+					<h3 className="sectionHeading" id="break-label">
+						Break Length
+					</h3>
 					<div className="arrows">
-						<button type="button" onClick={increaseBreak}>
+						<button type="button" onClick={increaseBreak} id="break-increment">
 							<i className="fa-solid fa-arrow-up" />
 						</button>
-						<h3>{breakLength}</h3>
-						<button type="button" onClick={decreaseBreak}>
+						<h3 id="break-length">{breakLength}</h3>
+						<button type="button" onClick={decreaseBreak} id="break-decrement">
 							<i className="fa-solid fa-arrow-down" />
 						</button>
 					</div>
 				</div>
 			</div>
-
-			<CountDown minutes={sessionLength} />
-			{
-				//{!isBreakInProgress ? (
-				//			<CountDown minutes={sessionLength} />
-				//		) : (
-				//			<CountDownBreak minutes={breakLength} />
-				//)}
-			}
+			<div id="countdown-timer">
+				<h3 id="timer-label">{isBreakInProgress ? "Break" : "Session"}</h3>
+				<p id="time-left">
+					{" "}
+					{`${
+						isBreakInProgress
+							? mb.toString().padStart(2, "0")
+							: m.toString().padStart(2, "0")
+					}:${isBreakInProgress ? sb.toString().padStart(2, "0") : s.toString().padStart(2, "0")}`}
+				</p>
+				{
+					// <div>{over ? "Break time!" : ""}</div>
+				}
+				<div>
+					<button
+						type="button"
+						id="start_stop"
+						onClick={() => {
+							const newPaused = !paused;
+							setPaused(newPaused);
+						}}
+					>
+						{paused ? "Start|Resume" : "Pause"}
+					</button>
+					<button type="button" id="reset" onClick={() => reset()}>
+						Reset
+					</button>
+				</div>
+			</div>
 		</>
 	);
 }
